95dee07e82d49b75af8b682574d975755ac908ad
anhptvnist <anhpt.vnist@gmail.com>
2020-12-15 22:24:24 +0700

    Them ban phim toc ky

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -1,54 +1,54 @@
-$(function(){
+$(function() {
     var $write = $('#write'),
         shift = false,
         capslock = false;
-     
-    $('#keyboard li').click(function(){
+
+    $('#keyboard li').click(function() {
         var $this = $(this),
             character = $this.html(); // If it's a lowercase letter, nothing happens to this variable
-         
+
         // Shift keys
         if ($this.hasClass('left-shift') || $this.hasClass('right-shift')) {
             $('.letter').toggleClass('uppercase');
             $('.symbol span').toggle();
-             
+
             shift = (shift === true) ? false : true;
             capslock = false;
             return false;
         }
-         
+
         // Caps lock
         if ($this.hasClass('capslock')) {
             $('.letter').toggleClass('uppercase');
             capslock = true;
             return false;
         }
-         
+
         // Delete
         if ($this.hasClass('delete')) {
             var html = $write.html();
-             
+
             $write.html(html.substr(0, html.length - 1));
             return false;
         }
-         
+
         // Special characters
         if ($this.hasClass('symbol')) character = $('span:visible', $this).html();
         if ($this.hasClass('space')) character = ' ';
         if ($this.hasClass('tab')) character = "\t";
         if ($this.hasClass('return')) character = "\n";
-         
+
         // Uppercase letter
         if ($this.hasClass('uppercase')) character = character.toUpperCase();
-         
+
         // Remove shift once a key is clicked.
         if (shift === true) {
             $('.symbol span').toggle();
             if (capslock === false) $('.letter').toggleClass('uppercase');
-             
+
             shift = false;
         }
-         
+
         // Add the character
         $write.html($write.html() + character);
     });
@@ -101,11 +101,19 @@ function myFunction(event) {
     }
     id = String(id)
     var button = document.getElementById(`${id}`)
+    var id2 = "letter" + " " + id
+    var button2 = document.getElementsByClassName(id2);
     button.style.backgroundColor = "red";
     setTimeout(() => {
         button.style.backgroundColor = "";
     }, 500)
     console.log(code)
     document.getElementById("key_pressed").value += String.fromCharCode(code) + '\n'
-};
 
+    if (button2.length) {
+        button2[button2.length - 1].style.backgroundColor = "blue";
+        setTimeout(() => {
+            button2[button2.length - 1].style.backgroundColor = '';
+        }, 500);
+    }
+};
