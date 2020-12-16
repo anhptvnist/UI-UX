5633b0e227d9e4921142be3bb569bccf8cd05d15
quangnt-vnist <quangnt.vnist@gmail.com>
2020-12-16 17:45:37 +0700

    add hoc am dau

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -180,3 +180,129 @@ function myFunction(event) {
 
 
 };
+
+
+var testAmDau = "";
+function checkAmDau(event) {
+    console.log("===========", event);
+    var code = event.keyCode;
+    var id = code;
+    if (code >= 48 && code < 59) {
+        var id = String(code - 48)
+    }
+    if (code == 8) {
+        var id = "delete"
+    }
+    if (code >= 65 && code <= 97) {
+        id = String.fromCharCode(code)
+    }
+    if (code == 219) {
+        id = "mongoac"
+    }
+    if (code == 220) {
+        id = "gachdoc"
+    }
+    if (code == 221) {
+        id = "dongngoac"
+    }
+    if (code == 186) {
+        id = "champhay"
+    }
+    if (code == 222) {
+        id = "ngoacdon"
+    }
+    if (code == 188) {
+        id = "phay"
+    }
+    if (code == 190) {
+        id = "cham"
+    }
+    if (code == 191) {
+        id = "gachcheo"
+    }
+    if (code == 16) {
+        id = "Shift"
+    }
+    if (code == 13) {
+        id = "Enter"
+    }
+    if (code == 20) {
+        id = "capslock"
+    }
+    id = String(id)
+    const convertKey = {
+        "81": "S",
+        "87": "K",
+        "69": "R",
+        82: "N",
+        84: "H",
+        85: "*",
+        73: "W",
+        79: "J",
+        80: "N",
+        219: "T",
+        65: "T",
+        83: "P",
+        68: "D",
+        70: "N",
+        71: "S",
+        74: "I",
+        75: "Y",
+        76: "J",
+        186: "G",
+        222: "K",
+        67: "U",
+        86: "O",
+        78: "E",
+        77: "A",
+        number: "#",
+
+    }
+    // Các phím mới nhập
+    if (event.keyCode < 59 && event.keyCode >= 48) {
+        steroCode = "number"
+    }
+    else steroCode = String(event.keyCode);
+    let time = new Date();
+
+    if (convertKey[`${steroCode}`]) {
+        let text = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds() + "." + time.getMilliseconds() + "    " + convertKey[`${steroCode}`] + "\n";
+        let textLogs = document.getElementById("text-logs");
+        textLogs.value += text;
+    }
+    test = event.key;
+    console.log(test);
+    let textCurrent = document.getElementById("text-current");
+    let checkIcon = document.getElementById("correct");
+    let falseIcon = document.getElementById("incorrect");
+    if (test === "a") {
+        checkIcon.style.display = "inline";
+        falseIcon.style.display = "none";
+        textCurrent.value = "a";
+        test = "";
+    }
+    else {
+        checkIcon.style.display = "none";
+        falseIcon.style.display = "inline"
+        textCurrent.value = "";
+    }
+
+
+    // Hết các phím mới nhập
+
+    var button = document.getElementById(`${id}`)
+    var id2 = "letter" + " " + id;
+
+    var button2 = document.getElementsByClassName(id2);
+    button.style.backgroundColor = "red";
+    setTimeout(() => {
+        button.style.backgroundColor = "";
+    }, 500)
+    button2[button2.length - 1].style.backgroundColor = "red";
+    setTimeout(() => {
+        button2[button2.length - 1].style.backgroundColor = '';
+    }, 500);
+    document.getElementById("key_pressed").value += String.fromCharCode(code) + '\n'
+
+
+};
